49c87f3bca080798952bcb81535cf790fe9a9c1a
Jose Claudivan <claudivan0103@gmail.com>
2021-02-02 20:59:10 -0300

    Finalizado e corrigido bug do codigo JS

--- detector-de-horario/script.js
+++ detector-de-horario/script.js
@@ -1,21 +1,28 @@
 function verificarHora() {
-    var mensagem = window.document.querySelector('#mensagem')
-    var imagem = window.document.querySelector('#imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    hora = 20
+  var mensagem = window.document.querySelector("#mensagem");
+  var imagem = window.document.querySelector("#imagem");
+  window.document
+    .getElementById("resultado")
+    .removeChild(window.document.querySelector("#botao"));
 
-    if (hora > 0 && hora < 12) {
-        mensagem.innerHTML = `Bom Dia! São exatamente <strong>${hora}:${minuto}.</strong>`
-        imagem.src = 'img/manha.jpg'
-    } else if(hora < 18) {
-        mensagem.innerHTML = `Boa Tarde! São exatamente <strong>${hora}:${minuto}.</strong>`
-        imagem.src = 'img/tarde.jpg'
-    } else {
-        mensagem.innerHTML = `Boa Noite! São exatamente <strong>${hora}:${minuto}.</strong>`
-        imagem.src = 'img/noite.jpg'
-    }
+  var data = new Date();
+  var hora = data.getHours();
+  var minuto = data.getMinutes();
+
+  if (hora > 0 && hora < 12) {
+    mensagem.innerHTML = `Bom Dia! São exatamente <strong>${hora}:${minuto}.</strong>`;
+    imagem.src = "img/manha.jpg";
+    window.document.body.style.background = '#fcf380'
+
+  } else if (hora < 18) {
+    mensagem.innerHTML = `Boa Tarde! São exatamente <strong>${hora}:${minuto}.</strong>`;
+    imagem.src = "img/tarde.jpg";
+    window.document.body.style.background = '#ffdd1f'
+
+  } else {
+    mensagem.innerHTML = `Boa Noite! São exatamente <strong>${hora}:${minuto}.</strong>`;
+    imagem.src = "img/noite.jpg";
+    window.document.body.style.background = '#636363'
+  }
 }
-
 
